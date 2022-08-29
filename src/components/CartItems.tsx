429d7a2a54c0a6314b7c3a9ceb337effd8607dd1
Oris John-Baptiste <orisjb@gmail.com>
2022-08-29 23:31:23 +0100

    added storybook

--- src/components/CartItems.tsx
+++ src/components/CartItems.tsx
@@ -2,13 +2,14 @@ import { FC, useContext, useId } from "react";
 import { AppContext } from "../context/context";
 import { ProductType } from "../types/types";
 import ProductCard from "./ProductCard";
+import styles from "../styles/CartItems.module.css";
 
 export const CartItems: FC = (product) => {
   let prefix = useId();
   const { state } = useContext(AppContext);
   const { shoppingCartProducts } = state;
   return (
-    <div className="cart-items">
+    <div className={styles.cartItems}>
       {shoppingCartProducts.map((product: ProductType) => (
         <ProductCard product={product} key={prefix+`_${Math.random() + product.id}`+product.name} />
       ))}
